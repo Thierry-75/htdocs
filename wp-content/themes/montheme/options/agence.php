546306564b14b9afcd4cd97267638a3e92872357
thierry dothee <thierrydothee@protonmail.com>
2022-04-27 16:18:26 +0200

    ajout css et js

--- wp-content/themes/montheme/options/agence.php
+++ wp-content/themes/montheme/options/agence.php
@@ -1,32 +1,50 @@
 <?php
 
-class AgenceMenuPage {
+class AgenceMenuPage
+{
 
-        const GROUP = 'agence_options';
+    const GROUP = 'agence_options';
 
     public static function register()
     {
-            add_action('admin_menu',[self::class,'addMenu']);
-            add_action('admin_init',[self::class, 'registerSettings']);
+        add_action('admin_menu', [self::class, 'addMenu']);
+        add_action('admin_init', [self::class, 'registerSettings']);
+        add_action('admin_enqueue_scripts', [self::class, 'registerScripts']);
+    }
+    // chargement librairie javascript et css sur backoffice 
+    public static function registerScripts($suffix)
+    {
+        if ($suffix === 'settings_page_agence_options') {
+            wp_register_style('flatpickr', 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css', [], false);
+            wp_register_script('flatpickr', 'https://cdn.jsdelivr.net/npm/flatpickr', [], false, true);
+            wp_register_script('montheme_admin', get_template_directory_uri() . '/assets/admin.js', ['flatpickr'], false, true);
+            wp_enqueue_script('montheme_admin');
+            wp_enqueue_style('flatpickr');
+        }
     }
 
     public static function registerSettings()
     {
-            register_setting(self::GROUP,'agence_horaire',['default'=>'agence fermée']);
-            add_settings_section('agence_options_section','paramètres',function () {
-                echo"Vous pouvez ici gérer les paramètres liés à l'agence";
+        register_setting(self::GROUP, 'agence_horaire', ['default' => 'agence fermée']);
+        register_setting(self::GROUP, 'agence_date');
+        add_settings_section('agence_options_section', 'paramètres', function () {
+            echo "Vous pouvez ici gérer les paramètres liés à l'agence";
         }, self::GROUP);
-           add_settings_field('agence_options_horaire',"Horaires d'ouverture",function(){
-                ?>
-                <textarea name="agence_horaire"  cols="30" rows="10" style="width: 100%"><?= esc_html(get_option('agence_horaire')) ?></textarea>
-                <?php
-           }, self::GROUP,'agence_options_section');   
-
+        add_settings_field('agence_options_horaire', "Horaires d'ouverture", function () {
+?>
+            <textarea name="agence_horaire" cols="30" rows="10" style="width: 100%"><?= esc_html(get_option('agence_horaire')) ?></textarea>
+        <?php
+        }, self::GROUP, 'agence_options_section');
+        add_settings_field('agence_options_date', "Date d'ouverture", function () {
+        ?>
+            <input type="text" name="agence_date" value="<?= esc_attr(get_option('agence_date')) ?>" class="montheme_datepicker">
+        <?php
+        }, self::GROUP, 'agence_options_section');
     }
 
     public static function addMenu()
     {
-        add_options_page("Gestion de l'agence","Agence","manage_options","agence_options",[self::class, 'render']);
+        add_options_page("Gestion de l'agence", "Agence", "manage_options", "agence_options", [self::class, 'render']);
     }
 
     public static function render()
@@ -35,11 +53,10 @@ class AgenceMenuPage {
         <h1>Gestion de l'agence</h1>
 
         <form action="options.php" method="post">
-            <?php settings_fields(self::GROUP ) ?>
+            <?php settings_fields(self::GROUP) ?>
             <?php do_settings_sections(self::GROUP); ?>
             <?php submit_button() ?>
         </form>
-        <?php
+<?php
     }
-
 }
